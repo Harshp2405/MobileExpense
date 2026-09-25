6335f40a691756e4eb4f36074ffcebe05cced3bb
harshp2405 <harshpanara7@gmail.com>
2026-09-25 12:09:57 +0530

    Babel change

--- babel.config.js
+++ babel.config.js
@@ -1,13 +1,17 @@
 module.exports = function (api) {
 	api.cache(true);
 	return {
-		presets: [
-			["babel-preset-expo", { jsxImportSource: "nativewind" }],
-			"nativewind/babel",
-		],
-		plugins: [
-			["inline-import", { extensions: [".sql"] }],
-			"react-native-reanimated/plugin"
-		],
-	};
+    presets: [
+      ["babel-preset-expo", { jsxImportSource: "nativewind" }],
+      "nativewind/babel",
+    ],
+    // plugins: [
+    // 	["inline-import", { extensions: [".sql"] }],
+    // 	"react-native-reanimated/plugin"
+    // ],
+    plugins: [
+      ["inline-import", { extensions: [".sql"] }],
+      "react-native-worklets/plugin",
+    ],
+  };
 };
